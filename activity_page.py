06893030921bd2ad4ce98ca15dcb5ecf4751f722
Jake <jakeclarey@gmail.com>
2025-07-27 22:33:42 -0400

    another test fix

--- activity_page.py
+++ activity_page.py
@@ -3,6 +3,7 @@ import tkinter as tk
 from PIL import Image, ImageTk
 import serial
 
+
 class ActivityPage(tk.Frame):
     def __init__(self, parent, controller):
         super().__init__(parent, width=1024, height=600, bg="#0032A0")
@@ -78,19 +79,20 @@ class ActivityPage(tk.Frame):
         self.set_user_info(
             self.controller.current_user_id, self.controller.current_user_credits
         )
-        self.ser = serial.Serial("/dev/ttyACM0", baudrate=38400, timeout=None)
-        self.ser.flush()
-        self.after(200, self.send_command("sort_early_exit\n"))
+
+        self.after(200, self.init_serial_and_send_command("sort_early_exit\n"))
         super().tkraise(aboveThis)
 
-    def send_command(self, command):
-            print(f"Sending {command}")
-            self.ser.write(command.encode())
-            while True:
-                response = self.ser.readline().decode()
-                print(response)
-                if response == "ACK\n":
-                    break
-                else:
-                    print("STM is not in the sort state, or is an unexpected state")
-                    break
+    def init_serial_and_send_command(self, command):
+        self.ser = serial.Serial("/dev/ttyACM0", baudrate=38400, timeout=None)
+        self.ser.flush()
+        print(f"Sending {command}")
+        self.ser.write(command.encode())
+        while True:
+            response = self.ser.readline().decode()
+            print(response)
+            if response == "ACK\n":
+                break
+            else:
+                print("STM is not in the sort state, or is an unexpected state")
+                break
